d3ad955afc85c5abbc9b91fdb51fadd61eda0ec6
Abdinoor Suleman <xarwaashe22@hotmail.com>
2020-03-30 18:24:51 +0300

    adding first themes

--- INSTTemp/wwwroot/js/site.js
+++ INSTTemp/wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your Javascript code.
+	/* ======= Sliding menu underline ====== */
+	// Menu has active item
+	//Ref: https://codepen.io/digistate/pen/OXXjXM
+
+	$nav = $("#navigation"),
+		$slideLine = $("#slide-line"),
+		$currentItem = $("#navigation li.active");
+
+
+	$(window).on('resize load', function () {
+
+		if ($currentItem[0]) {
+			$slideLine.css({
+				"width": $currentItem.width(),
+				"left": $currentItem.position().left
+			});
+		}
+	});
+
+	// Underline transition
+	$nav.find("li").hover(
+		// Hover on
+		function () {
+			$slideLine.css({
+				"width": $(this).width(),
+				"left": $(this).position().left
+			});
+		},
+		// Hover out
+		function () {
+			if ($currentItem[0]) {
+				// Go back to current
+				$slideLine.css({
+					"width": $currentItem.width(),
+					"left": $currentItem.position().left
+				});
+			} else {
+				// Disapear
+				$slideLine.width(0);
+			}
+		}
+	);
+
+
+});
